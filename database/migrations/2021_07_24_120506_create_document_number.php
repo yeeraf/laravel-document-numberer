fd8b68acce7585ef891d7cc1c3b45c28f6a03382
Mn <mininoz@gmail.com>
2021-07-24 22:59:47 +0700

    fix psr-4 and move migration files

---
+++ database/migrations/2021_07_24_120506_create_document_number.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentNumber extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('document_numbers', function (Blueprint $table) {
+            $table->id();
+            $table->string("prefix");
+            $table->string("suffix");
+            $table->integer("pad_length");
+            $table->string("pad_string");
+            $table->string("pad_type")->nullable();
+            $table->unsignedInteger("current_number");
+            $table->timestamps();
+
+            // $table->unique(['prefix', 'suffix', 'pad_length', 'pad_string', 'pad_type'], 'dn_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('document_numbers');
+    }
+}
